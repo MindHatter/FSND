19c30b1d81006addaad09618c02d6d3907769832
Aleksandr Menshikov <menshikov.abz@gmail.com>
2020-10-23 23:22:33 +0300

    Postman is configured. API is checked

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -27,7 +27,24 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks', methods=['GET'], endpoint='get_drinks')
+def drinks():
+    """
+    Public permission
+    This API fetches all drinks with a short description
+    Return the drinks array or the error handler
+    """
+    try:
+        return json.dumps({
+            'success': True,
+            'drinks': [drink.short() for drink in Drink.query.all()]
+        }), 200
+    except Exception as e:
+        print(e)
+        return json.dumps({
+            'success': False,
+            'error': "An error occurred"
+        }), 500
 
 '''
 @TODO implement endpoint
@@ -37,7 +54,26 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks-detail', methods=['GET'], endpoint='drinks_detail')
+@requires_auth('get:drinks-detail')
+def drinks_detail(f):
+    """
+        Public permission
+        This API fetches all drinks with a long description
+        Return the drinks array or the error handler
+        """
+    try:
+        return json.dumps({
+            'success':
+            True,
+            'drinks': [drink.long() for drink in Drink.query.all()]
+        }), 200
+    except Exception as e:
+        print(e)
+        return json.dumps({
+            'success': False,
+            'error': "An error occurred"
+        }), 500
 
 '''
 @TODO implement endpoint
@@ -48,7 +84,28 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['POST'], endpoint='post_drink')
+@requires_auth('post:drinks')
+def drinks(f):
+    """
+     post:drinks permission
+     This API creates a new drink and returns its long description
+     Return the created drink info or the error handler
+     """
 
+    data = dict(request.form or request.json or request.data)
+    drink = Drink(title=data.get('title'),
+                  recipe=data.get('recipe') if type(data.get('recipe')) == str
+                  else json.dumps(data.get('recipe')))
+    try:
+        drink.insert()
+        return json.dumps({'success': True, 'drink': drink.long()}), 200
+    except Exception as e:
+        print(e)
+        return json.dumps({
+            'success': False,
+            'error': "An error occurred"
+        }), 500
 
 '''
 @TODO implement endpoint
@@ -61,7 +118,39 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks/<id>', methods=['PATCH'], endpoint='patch_drink')
+@requires_auth('patch:drinks')
+def drinks(f, id):
+    """
+     patch:drinks permission
+     This API updates a drink if it exists
+     Return the updated drink info or the error handler
+     OBS: I would rather return a single object instead of an array, but POSTman test once again enforces it
+     """
+    try:
+        data = dict(request.form or request.json or request.data)
+        drink = drink = Drink.query.filter(Drink.id == id).one_or_none()
+        if drink:
+            drink.title = data.get('title') if data.get(
+                'title') else drink.title
+            recipe = data.get('recipe') if data.get('recipe') else drink.recipe
+            drink.recipe = recipe if type(recipe) == str else json.dumps(
+                recipe)
+            drink.update()
+            return json.dumps({'success': True, 'drinks': [drink.long()]}), 200
+        else:
+            return json.dumps({
+                'success':
+                False,
+                'error':
+                'Drink #' + id + ' not found to be edited'
+            }), 404
+    except Exception as e:
+        print(e)
+        return json.dumps({
+            'success': False,
+            'error': "An error occurred"
+        }), 500
 
 '''
 @TODO implement endpoint
@@ -73,7 +162,32 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks/<id>', methods=['DELETE'], endpoint='delete_drink')
+@requires_auth('patch:drinks')
+def drinks(f, id):
+    """
+     delete:drinks permission
+     This API deletes a drink if it exists
+     Return the deleted drink info or the error handler
+     """
+    try:
+        drink = drink = Drink.query.filter(Drink.id == id).one_or_none()
+        if drink:
+            drink.delete()
+            return json.dumps({'success': True, 'drink': id}), 200
+        else:
+            return json.dumps({
+                'success':
+                False,
+                'error':
+                'Drink #' + id + ' not found to be deleted'
+            }), 404
+    except Exception as e:
+        print(e)
+        return json.dumps({
+            'success': False,
+            'error': "An error occurred"
+        }), 500
 
 ## Error Handling
 '''
@@ -97,14 +211,40 @@ def unprocessable(error):
                     }), 404
 
 '''
-
+@app.errorhandler(404)
+def unprocessable(error):
+    """
+     Propagates the formatted 404 error to the response
+     """
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
 '''
 @TODO implement error handler for 404
     error handler should conform to general task above 
 '''
-
+@app.errorhandler(500)
+def unprocessable(error):
+    """
+     Propagates the formatted 404 error to the response
+     """
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "Server error"
+    }), 500
 
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above 
 '''
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    """
+    Receive the raised authorization error and propagates it as response
+    """
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
